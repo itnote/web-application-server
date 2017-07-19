b37d16217e6cc60a50db9cba3ea59779e75c8d77
joshua <jgchoi.qa@gmail.com>
2017-07-19 16:27:12 +0900

    POST로 회원가입하도록 변경 (요구사항 3)

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -5,6 +5,7 @@ import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.Map;
 
 import db.DataBase;
@@ -12,13 +13,13 @@ import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.HttpRequestUtils;
+import util.IOUtils;
+import static util.HttpRequestUtils.Pair;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
     private Socket connection;
-
-    private StringBuffer request = new StringBuffer();
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -33,13 +34,28 @@ public class RequestHandler extends Thread {
 
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
 
-            String requestHeader = br.readLine();
+            Map<String, String> headerList = new HashMap<>();
 
-            String[] requestParameter = requestHeader.split(" ");
-            if(requestParameter[1].contains("?")) {
-                String[] queryString = requestParameter[1].split("\\?");
+            String line = br.readLine();
+            String[] requestParameter = line.split(" ");
 
-                Map<String, String> query = HttpRequestUtils.parseQueryString(queryString[1]);
+            while(!"".equals(line)) {
+                Pair pair = HttpRequestUtils.parseHeader(line);
+                if(pair != null) {
+                    headerList.put(pair.getKey(), pair.getValue());
+                }
+                line = br.readLine();
+            }
+
+            log.debug(headerList.toString());
+            String requestBody = "";
+            if(headerList.containsKey("Content-Length")) {
+                requestBody = IOUtils.readData(br, Integer.parseInt(headerList.get("Content-Length")));
+                log.debug(requestBody);
+            }
+
+            if(!requestBody.equals("")) {
+                Map<String, String> query = HttpRequestUtils.parseQueryString(requestBody);
 
                 if(DataBase.findUserById(query.get("userId")) == null && createUser(query)) {
                     log.debug(DataBase.findUserById(query.get("userId")).toString());
@@ -84,7 +100,7 @@ public class RequestHandler extends Thread {
         String name = query.get("name");
         String email = query.get("email");
 
-        if(!userId.isEmpty() && !password.isEmpty() && !name.isEmpty() && !email.isEmpty()) {
+        if(userId != null && password != null && name != null && email != null) {
             User user = new User(userId, password, name, email);
             DataBase.addUser(user);
             log.debug(String.valueOf(user.hashCode()));
